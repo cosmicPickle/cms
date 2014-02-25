c71c7e076e1416976727ca3d85ff257dc4460a31
Teodor Klissarov <teodorklissarov@gmail.com>
2014-02-25 21:02:40 +0200

    DB Update

--- cms.sql
+++ cms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 25, 2014 at 06:54 PM
+-- Generation Time: Feb 25, 2014 at 07:00 PM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.16
 
